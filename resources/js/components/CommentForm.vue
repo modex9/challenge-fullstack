802868ef9b9c69475cf12fda628be7a3790469cd
modex9 <modestas.slivinskas@gmail.com>
2020-08-09 14:25:45 +0300

    created comment form component

---
+++ resources/js/components/CommentForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <form method="POST" @submit="saveComment()">
+            <input type="hidden" name="_token" :value="csrf">
+            <div class="form-group row">
+                <label for="name" class="col-md-4 col-form-label text-md-right">Add a comment</label>
+                <div class="col-md-6">
+                    <input v-model="content" id="content" type="text" class="form-control" :class="{ 'is-invalid' : 'content' in errors}" name="content" value="" required autocomplete="name" autofocus>
+                    <span v-for="error in errors" class="invalid-feedback" role="alert">
+                        <strong>{{ error[0] }}</strong>
+                    </span>
+                </div>
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <p v-if="!user">You must <a href="#" @click="$emit('show-login')">login to</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'CommentForm',
+        props : ['saveCommentRoute', 'csrf', 'user'],
+        data : function() {
+            return {
+                content : '',
+                errors : {},
+            }
+        },
+        methods : {
+            saveComment() {
+                event.preventDefault();
+                fetch(this.saveCommentRoute, {
+                    method : 'POST',
+                    body : JSON.stringify({
+                        'content' : this.content,
+                    }),
+                    headers : {
+                        "Content-Type": "application/json; charset=utf-8",
+                        'X-CSRF-TOKEN' : this.csrf,
+                    },
+                })
+                .then(data => data.json())
+                .then(data => {
+                    if(!data['success'])
+                        this.errors = data['errors'];
+                    else {
+                        this.$emit('new-comment', data['comment']);
+                    }
+                })
+                .catch(error => console.log(error));
+            },
+        }
+    }
+</script>
